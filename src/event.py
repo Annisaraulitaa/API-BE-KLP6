89b1e560807558a9b1776c8d50036a6e5e2c12e2
Annisaraulitaa <annisaraulita240702@gmail.com>
2024-06-10 11:33:19 +0800

    feat: add event endpoint

---
+++ src/event.py
@@ -0,0 +1,106 @@
+from datetime import datetime
+import uuid
+from flask import Blueprint, jsonify, request
+from src.database import Event, db
+import src.constants.http_status_codes as http
+from src.utils.upload_image_to_firebase import upload_image_to_firebase
+from src.utils.delete_image_from_firebase import delete_image_from_firebase
+
+event = Blueprint('event', __name__)
+
+@event.get('/')
+def get_all():
+
+    events = Event.query.all()
+    
+    return jsonify([event.to_dict() for event in events]), http.HTTP_200_OK
+
+@event.get('/<id>')
+def get_by_id(id):
+    event = Event.query.filter_by(id=id).first()
+    
+    if not event:
+        return jsonify({"msg": "Event not found"}), http.HTTP_404_NOT_FOUND
+    
+    return jsonify(event.to_dict()), http.HTTP_200_OK
+
+@event.post('/create')
+def create():
+    title = request.form.get('title', None)
+    content = request.form.get('content', None)
+    photo = request.form.get('photo', None)
+    created_by = request.form.get('created_by', None)
+    
+    if not title or not content or not photo or not created_by:
+        return jsonify({"msg": "All fields must be provided"}), http.HTTP_400_BAD_REQUEST
+    
+    new_id = str(uuid.uuid4())
+    photo_url = upload_image_to_firebase(photo, file_name=f'event/{new_id}')
+    if not photo_url[0]:
+        return jsonify({
+            'msg': photo_url[1]
+        }), http.HTTP_500_INTERNAL_SERVER_ERROR
+
+    created_at = datetime.now().timestamp() * 1000   
+
+    event = Event(
+        id=new_id,
+        title=title,
+        content=content,
+        photo_url=photo_url,
+        created_by=created_by,
+        updated_at=created_at,
+        created_at=created_at
+    )
+    
+    db.session.add(event)
+    db.session.commit()
+    
+    return jsonify(event.to_dict()), http.HTTP_201_CREATED
+
+@event.put('/update/<id>')
+def update(id):
+    title = request.form.get('title', None)
+    content = request.form.get('content', None)
+    photo = request.form.get('photo', None)
+    
+    event: Event = Event.query.get(id)
+    
+    if not event:
+        return jsonify({"msg": "Event not found"}), http.HTTP_404_NOT_FOUND
+    
+    if title:
+        event.title = title
+    if content:
+        event.content = content
+    if photo:
+        photo_url = upload_image_to_firebase(photo, file_name=f'event/{id}')
+        if not photo_url[0]:
+            return jsonify({
+                'msg': photo_url[1]
+            }), http.HTTP_500_INTERNAL_SERVER_ERROR
+        else:
+            event.photo_url = photo_url[0]
+    
+    event.updated_at = datetime.now().timestamp() * 1000
+    
+    db.session.commit()
+    
+    return jsonify(event.to_dict()), http.HTTP_200_OK
+
+@event.delete('/delete/<id>')
+def delete(id):
+    event: Event = Event.query.get(id)
+    
+    if not event:
+        return jsonify({"msg": "Event not found"}), http.HTTP_404_NOT_FOUND
+    
+    delete = delete_image_from_firebase(event.photo_url)
+
+    if not delete[0]:
+        return jsonify({"msg": delete[1]}), http.HTTP_500_INTERNAL_SERVER_ERROR
+    
+    db.session.delete(event)
+    db.session.commit()
+    
+    return jsonify({"msg": "Event deleted"}), http.HTTP_200_OK
